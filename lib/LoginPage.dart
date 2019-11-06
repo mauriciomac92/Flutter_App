ef374b1c9d3c1a5b6ce1b516761a724fe8dc3501
mauriciomac92 <36291153+mauriciomac92@users.noreply.github.com>
2019-11-05 19:52:36 -0800

    Update loginPage

--- lib/LoginPage.dart
+++ lib/LoginPage.dart
@@ -1,73 +1,80 @@
 import 'package:flutter/material.dart';
-import 'package:software_engineer/PageLayout.dart';
+import 'package:testlogin/auth.dart';
+//import 'package:firebase_auth/firebase_auth.dart';
+//import 'package:cloud_firestore/cloud_firestore.dart';
+//import 'nextPage.dart';
 
 class LoginPage extends StatefulWidget {
+    static final String id = 'Login_Page';
+  // This widget is the root of your application.
   @override
-  LoginState createState() =>
-      LoginState(); // => short for: {return LoginState()}
+  State<StatefulWidget> createState() => _LoginPageState();
 }
 
-class LoginState extends State<LoginPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        resizeToAvoidBottomPadding: true,
-        body: Center(
-            child: Container(
-          color: Colors.grey,
-          child: Padding(
-              padding: const EdgeInsets.all(30.0),
-              child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Image.asset(
-                      'assets/images/fullerton_logo.png',
-                      height: 150.0,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(10.0),
-                    ),
-                    OutlineButton(
-                        splashColor: Colors.white,
-                        onPressed: () {
-                          /* original push without google sign */
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => mainPage())); //mainpage
-                          /* with google sign in
-                          signInWithGoogle().whenComplete(() {
-                            Navigator.of(context).push(
-                              MaterialPageRoute(builder: (context) {
-                                return mainPage();
-                              }),
-                            );
-                          }); */
-                        },
-                        borderSide: BorderSide(color: Colors.black),
-                        child: Row(
-                          mainAxisSize: MainAxisSize.min,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            Image.asset(
-                              'assets/images/google_logo.png',
-                              height: 45.0,
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(left: 10),
-                              child: Text(
-                                'Sign In With Google',
-                                style: TextStyle(
-                                  fontSize: 20,
-                                ),
-                              ),
-                            )
-                          ],
-                        ),
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(30.0))),
-                  ])),
-        )));
+class _LoginPageState extends State<LoginPage> {
+final formKey = GlobalKey<FormState>();
+String _email;
+String _password;
+void _submit(){
+  if(formKey.currentState.validate()){
+    formKey.currentState.save();
+    // print(_email);
+    // print(_password);
+    Auth.login(_email, _password);
   }
 }
+  @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        resizeToAvoidBottomPadding: true,
+        body: SingleChildScrollView(
+          child: Container(
+            height: MediaQuery.of(context).size.height,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  "Login In"
+                ),
+                Form(key: formKey,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: TextFormField(
+                        decoration: InputDecoration(labelText: 'Email'),
+                        validator: (input) => !input.contains('@') ? 'Please enter a valid email' : null,
+                        onSaved: (input) => _email = input,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: TextFormField(
+                        decoration: InputDecoration(labelText: 'Password'),
+                        validator: (input) => input.length < 6 ? 'Must be at lease 6 characters' : null,
+                        onSaved: (input) => _password = input,
+                        obscureText: true,
+                      ),
+                    ),
+                    SizedBox(height: 20.0),
+                    Container(
+                      child: FlatButton(
+                        onPressed: _submit,
+                        child: Text(
+                          'Login',
+                        ),
+
+                      ),
+                    )
+                  ],
+                ))
+              ],
+            ),
+          ),
+        ),
+        );
+    }
+}
+
