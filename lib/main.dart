4142a933595c3dde581ba54018d4ae8322c6737e
mauriciomac92 <36291153+mauriciomac92@users.noreply.github.com>
2019-11-05 19:50:51 -0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,45 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:software_engineer/GoogleLogin.dart';
+import 'package:testlogin/welcomeScreen.dart';
+import 'welcomeScreen.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  // Widget _getScreenId(){
+  //   return StreamBuilder<FirebaseUser>(
+  //     stream: FirebaseAuth.instance.onAuthStateChanged,
+  //     builder: (BuildContext context, snapshot){
+  //       if(snapshot.hasData){
+  //         return Layout();
+  //       }
+  //       else{
+  //         return WelcomePage();
+  //       }
+  //     },
+  //   );
+  // }
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "Flutter Messenger",
-      theme: ThemeData.dark().copyWith(
-        primaryColor: Colors.blueGrey,
-        scaffoldBackgroundColor: Colors.grey[20],
-        accentColor: Colors.white,
-      ),
-      home: StreamBuilder<Object>(
-          stream: null,
-          builder: (context, snapshot) {
-            return GoogleLogin();
-          }),
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Flutter Messenger',
+          theme: ThemeData.dark().copyWith(
+            primaryColor: Colors.blueGrey,
+            scaffoldBackgroundColor: Colors.grey[20],
+            accentColor: Colors.white,
+          ),
+          home: StreamBuilder<FirebaseUser>( // if logged in then use app if not logged in start at welcome screen
+            stream: FirebaseAuth.instance.onAuthStateChanged,
+            builder: (BuildContext context, snapshot){
+              if(snapshot.hasData){
+              return WelcomePage();
+              } else {
+                return WelcomePage();
+              }
+            }),         
     );
   }
 }
+
