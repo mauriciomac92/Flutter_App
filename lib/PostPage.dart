acd3272d8d6b117dbb83e9d5d1bb61586b1bf713
mauriciomac92 <36291153+mauriciomac92@users.noreply.github.com>
2019-11-06 10:44:12 -0800

    connect to firebase post page

--- lib/PostPage.dart
+++ lib/PostPage.dart
@@ -1,7 +1,10 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:software_engineer/MessagePage.dart';
-import 'package:software_engineer/PageLayout.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+final _firestore = Firestore.instance;
 
 class PostsPage extends StatefulWidget {
   @override
@@ -9,160 +12,81 @@ class PostsPage extends StatefulWidget {
 }
 
 class _PostsPage extends State<PostsPage> {
-  List<int> post = List.generate(10, (i) => i);
-  ScrollController scroller = new ScrollController();
-  bool reloading = false;
-
-  @override
-  void initState() {
-    super.initState();
-    scroller.addListener(() {
-      if (scroller.position.pixels == scroller.position.maxScrollExtent) {
-        getMoreData();
+//  void getPosts() async {
+//    final posts = await _firestore.collection('posts').getDocuments();
+//    for (var post in posts.documents) {
+//      print(post.data);
+//    }
+//  }
+  void postsStream() async {
+    await for (var snapshot in _firestore.collection('posts').snapshots()) {
+      for (var post in snapshot.documents) {
+        print(post.data);
       }
-    });
-  }
-
-  @override
-  void dispose() {
-    scroller.dispose();
-    super.dispose();
-  }
-
-  getMoreData() async {
-    if (!reloading) {
-      setState(() => reloading = true);
-      List<int> newData = await pullData(post.length, post.length + 10);
-      if (newData.isEmpty) {
-        double edge = 50.0;
-        double offsetFromBottom =
-            scroller.position.maxScrollExtent - scroller.position.pixels;
-        if (offsetFromBottom < edge) {
-          scroller.animateTo(scroller.offset - (edge - offsetFromBottom),
-              duration: new Duration(milliseconds: 500), curve: Curves.easeOut);
-        }
-      }
-      setState(() {
-        post.addAll(newData);
-        reloading = false;
-      });
     }
-  }
-
-  Widget createReloadAnimation() {
-    return new Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: new Center(
-        child: new Opacity(
-          opacity: reloading ? 1.0 : 0.0,
-          child: new CircularProgressIndicator(),
-        ),
-      ),
-    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text('Post'),
-        leading: IconButton(
-          icon: const Icon(Icons.menu),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.add),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => mainPage()),
-              );
-            },
-          )
-        ],
-      ),
-      body: ListView.builder(
-        itemCount: post.length + 1,
-        itemBuilder: (context, index) {
-          if (index == post.length) {
-            return createReloadAnimation();
-          } else {
-            return Column(
-              children: <Widget>[
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.all(1.0),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: <Widget>[
-                          Padding(
-                            padding: const EdgeInsets.all(5.0),
-                            child: Icon(
-                              Icons.account_circle,
-                              size: 50.0,
-                              color: Colors.blueGrey,
-                            ),
-                          ),
-                          Text(
-                            "$index m",
-                            style: TextStyle(color: Colors.grey),
-                          ),
-                        ],
-                      ),
-                    ),
-                    new Flexible(
-                      child: new Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Padding(
-                              padding: const EdgeInsets.fromLTRB(
-                                  0.0, 12.0, 12.0, 0.0),
-                              child: Text(
-                                "User/Club # $index",
-                                style: TextStyle(
-                                    fontSize: 18.0,
-                                    fontWeight: FontWeight.bold,
-                                    decoration: TextDecoration.underline),
-                              ),
-                            ),
-                            Container(
-                              padding: const EdgeInsets.fromLTRB(
-                                  0.0, 10.0, 12.0, 6.0),
-                              width: 300.0,
-                              child: new Column(
-                                children: <Widget>[
-                                  new Text(
-                                      "This is where posts will gooooooooooo fbegbhegherkghkjerwhg wekrhgfkwjerhgkwrehg rgkherwkghwe",
-                                      textAlign: TextAlign.left),
-                                ],
-                              ),
-                            )
-                          ]),
-                    )
-                  ],
-                ),
-                Divider(
-                  height: 2.0,
-                  color: Colors.grey,
-                )
-              ],
+    return Scaffold(
+        body: SafeArea(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+          PostsStream(),
+        ])));
+  }
+}
+
+class PostsStream extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+        stream: _firestore.collection('posts').snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return Center(
+              child: CircularProgressIndicator(
+                backgroundColor: Colors.lightBlueAccent,
+              ),
             );
           }
-        },
-        controller: scroller,
-      ),
-    );
-  }
+          final posts = snapshot.data.documents;
+          List<PostsBorder> postsBorders = [];
+          for (var post in posts) {
+            final postText = post.data['text'];
+            final postUser = post.data['user'];
 
-  Future<List<int>> pullData(int currentLength, int newLength) async {
-    return Future.delayed(Duration(seconds: 2), () {
-      return List.generate(newLength - currentLength, (i) => i + currentLength);
-    });
+            final postBorder = PostsBorder(
+              user: postUser,
+              text: postText,
+            );
+            postsBorders.add(postBorder);
+          }
+          return Expanded(
+            child: ListView(
+              children: postsBorders,
+            ),
+          );
+        });
+  }
+}
+
+class PostsBorder extends StatelessWidget {
+  PostsBorder({this.user, this.text});
+  final String user;
+  final String text;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.all(1.0),
+        child: Material(
+          color: Colors.blueGrey,
+          child: Text(
+            '$text from $user',
+            style: TextStyle(fontSize: 30.0),
+          ),
+        ));
   }
 }
