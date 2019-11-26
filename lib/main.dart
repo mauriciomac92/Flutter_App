6f5ecebb9985103d9fd9972df1fff4ab7ec56622
mauriciomac92 <36291153+mauriciomac92@users.noreply.github.com>
2019-11-26 13:49:01 -0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:testlogin/PageLayout.dart';
-import 'package:testlogin/loginPage.dart';
+import 'PageLayout.dart';
+import 'loginPage.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,14 +9,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
         return MaterialApp(
-          debugShowCheckedModanner: false,
+          debugShowCheckedModeBanner: false,
           title: 'Flutter Messenger',
           theme: ThemeData.dark().copyWith(
             primaryColor: Colors.blueGrey,
             scaffoldBackgroundColor: Colors.grey[20],
             accentColor: Colors.white,
           ),
-          home: StreamBuilder<FirebaseUser>( // if logged in then use app if not logged in start at welcome screen
+          home: StreamBuilder<FirebaseUser>( // if logged in then use app if not logged in start at login screen
             stream: FirebaseAuth.instance.onAuthStateChanged,
             builder: (BuildContext context, snapshot){
               if(snapshot.hasData){
@@ -28,3 +28,4 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
