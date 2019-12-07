22163364fc2be55c35a08f043ac5a5b910bc3c9f
laliucon <43503101+laliucon@users.noreply.github.com>
2019-12-06 17:27:00 -0800

    Add files via upload

---
+++ lib/DatabaseService.dart
@@ -0,0 +1,87 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'User.dart';
+import 'constants.dart';
+
+class DatabaseService {
+  static void updateUser(User user) {
+    usersRef.document(user.id).updateData({
+      'username': user.username,
+      'profileImageUrl': user.profileImageUrl,
+      'bio': user.bio,
+      'name': user.name,
+      'phone': user.phone,
+    });
+  }
+
+  static Future<QuerySnapshot> searchUsers(String username) {
+    Future<QuerySnapshot> users =
+    usersRef.where('username', isGreaterThanOrEqualTo: username).getDocuments();
+    return users;
+  }
+
+  static void followUser({String currentUserId, String userId}) {
+    // Add user to current user's following collection
+    followingRef
+        .document(currentUserId)
+        .collection('userFollowing')
+        .document(userId)
+        .setData({});
+    // Add current user to user's followers collection
+    followersRef
+        .document(userId)
+        .collection('userFollowers')
+        .document(currentUserId)
+        .setData({});
+  }
+
+  static void unfollowUser({String currentUserId, String userId}) {
+    // Remove user from current user's following collection
+    followingRef
+        .document(currentUserId)
+        .collection('userFollowing')
+        .document(userId)
+        .get()
+        .then((doc) {
+      if (doc.exists) {
+        doc.reference.delete();
+      }
+    });
+    // Remove current user from user's followers collection
+    followersRef
+        .document(userId)
+        .collection('userFollowers')
+        .document(currentUserId)
+        .get()
+        .then((doc) {
+      if (doc.exists) {
+        doc.reference.delete();
+      }
+    });
+  }
+
+  static Future<bool> isFollowingUser(
+      {String currentUserId, String userId}) async {
+    DocumentSnapshot followingDoc = await followersRef
+        .document(userId)
+        .collection('userFollowers')
+        .document(currentUserId)
+        .get();
+    return followingDoc.exists;
+  }
+
+  static Future<int> numFollowing(String userId) async {
+    QuerySnapshot followingSnapshot = await followingRef
+        .document(userId)
+        .collection('userFollowing')
+        .getDocuments();
+    return followingSnapshot.documents.length;
+  }
+
+  static Future<int> numFollowers(String userId) async {
+    QuerySnapshot followersSnapshot = await followersRef
+        .document(userId)
+        .collection('userFollowers')
+        .getDocuments();
+    return followersSnapshot.documents.length;
+  }
+}
