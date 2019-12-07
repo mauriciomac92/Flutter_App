c97d18ec9c7ff62f4317ac7aff0ac38d4f6b8691
laliucon <43503101+laliucon@users.noreply.github.com>
2019-12-06 17:16:56 -0800

    Update ProfilePage.dart

--- lib/ProfilePage.dart
+++ lib/ProfilePage.dart
@@ -1,185 +1,259 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'User_data.dart';
+import 'package:software_engineer/User.dart';
+import 'edit_profile_page.dart';
+import 'DatabaseService.dart';
+import 'constants.dart';
+import 'package:provider/provider.dart';
 
 
 class ProfilePage extends StatefulWidget {
+  final String currentUserId;
+  final String userId;
+
+  ProfilePage({this.currentUserId, this.userId});
 
   @override
   _ProfilePageState createState() => _ProfilePageState();
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  bool isFollowing = false;
+  int followerCount = 0;
+  int followingCount = 0;
+  int postCount = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _setupIsFollowing();
+    _setupFollowers();
+    _setupFollowing();
+  }
+
+  _setupIsFollowing() async {
+    bool isFollowingUser = await DatabaseService.isFollowingUser(
+      currentUserId: widget.currentUserId,
+      userId: widget.userId,
+    );
+    setState(() {
+      isFollowing = isFollowingUser;
+    });
+  }
+
+  _setupFollowers() async {
+    int userFollowerCount = await DatabaseService.numFollowers(widget.userId);
+    setState(() {
+      followerCount = userFollowerCount;
+    });
+  }
+
+  _setupFollowing() async {
+    int userFollowingCount = await DatabaseService.numFollowing(widget.userId);
+    setState(() {
+      followingCount = userFollowingCount;
+    });
+  }
+
+  _followOrUnfollow() {
+    if (isFollowing) {
+      _unfollowUser();
+    } else {
+      _followUser();
+    }
+  }
+
+  _unfollowUser() {
+    DatabaseService.unfollowUser(
+      currentUserId: widget.currentUserId,
+      userId: widget.userId,
+    );
+    setState(() {
+      isFollowing = false;
+      followerCount--;
+    });
+  }
+
+  _followUser() {
+    DatabaseService.followUser(
+      currentUserId: widget.currentUserId,
+      userId: widget.userId,
+    );
+    setState(() {
+      isFollowing = true;
+      followerCount++;
+    });
+  }
+
+  _displayButton(User user) {
+    return user.id == Provider.of<UserData>(context).currentUserId
+        ? Container(
+      width: 200.0,
+      child: FlatButton(
+        onPressed: () => Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (_) => EditProfileScreen(
+              user: user,
+            ),
+          ),
+        ),
+        color: Colors.blue,
+        textColor: Colors.white,
+        child: Text(
+          'Edit Profile',
+          style: TextStyle(fontSize: 18.0),
+        ),
+      ),
+    )
+        : Container(
+      width: 200.0,
+      child: FlatButton(
+        onPressed: _followOrUnfollow,
+        color: isFollowing ? Colors.grey[200] : Colors.blue,
+        textColor: isFollowing ? Colors.black : Colors.white,
+        child: Text(
+          isFollowing ? 'Unfollow' : 'Follow',
+          style: TextStyle(fontSize: 18.0),
+        ),
+      ),
+    );
+  }
+
+  Column BuildStatColumn (String label, int number) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          number.toString(),
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 18.0,
+          ),
+        ),
+        SizedBox(height: 5.0),
+        Text(
+          label,
+          style: TextStyle(
+              fontFamily: 'Montserrat',
+              color: Colors.grey),
+        )
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0.0,
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back),
-          color: Colors.black,
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-        ),
         title: Text('Profile'),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.more_vert),
-          )
-        ],
         centerTitle: true,
       ),
-      body: ListView(
-        children: <Widget>[
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Center(child: CircleAvatar(
-                // place for user to add their avatar
-                radius: 70,
-                backgroundColor: Colors.grey,
-                child: Icon(Icons.add_a_photo),
-              ),
-              ),
-              SizedBox(height: 25.0),
-              Text(
-                'Username',
-                style: TextStyle(
-                    fontFamily: 'Montserrat',
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 4.0),
-              Text(
-                'Bio',
-                style: TextStyle(fontFamily: 'Montserrat', color: Colors.grey),
-              ),
-              Padding(
-                padding: EdgeInsets.all(30.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      body: FutureBuilder(
+          future: usersRef.document(widget.userId).get(),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (!snapshot.hasData) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            User user = User.fromDoc(snapshot.data);
+            return ListView(
+              children: <Widget>[
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Text(
-                          '24K',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                          fontSize: 18.0,
+                    Center(
+                      child: CircleAvatar(
+                        // place for user to add their avatar
+                        radius: 70,
+                        backgroundColor: Colors.grey,
+                        backgroundImage: user.profileImageUrl.isEmpty
+                            ? AssetImage('assets/images/user_placeholder.jpg')
+                            : CachedNetworkImageProvider(user.profileImageUrl),
+                      ),
+                    ),
+                    SizedBox(height: 25.0),
+                    Text(
+                      user.username,
+                      style: TextStyle(
+                          fontFamily: 'Montserrat',
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 4.0),
+                    Text(
+                      user.bio,
+                      style: TextStyle(
+                          fontFamily: 'Montserrat', color: Colors.grey),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(30.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          BuildStatColumn("Post", postCount),
+                          BuildStatColumn("Follower", followerCount),
+                          BuildStatColumn("Following", followingCount),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(8.0),
+                    ),
+                    Card(
+                      // place for user to input their name
+                      child: ListTile(
+                        leading: Icon(Icons.account_circle),
+                        title: Container(
+                          child: Text(
+                            user.name,
+                            style: TextStyle(fontSize: 18.0),
                           ),
                         ),
-                        SizedBox(height: 5.0),
-                        Text(
-                          'FOLLOWERS',
-                          style: TextStyle(
-                              fontFamily: 'Montserrat',
-                              color: Colors.grey),
-                        )
-                      ],
+                      ),
                     ),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Text(
-                          '31',
-                          style: TextStyle(
-                              fontSize: 18.0,
-                              fontWeight: FontWeight.bold),
-                        ),
-                        SizedBox(height: 5.0),
-                        Text(
-                          'Posts',
-                          style: TextStyle(
-                              fontFamily: 'Montserrat',
-                              color: Colors.grey),
-                        )
-                      ],
+                    Padding(
+                      padding: EdgeInsets.all(12.0),
                     ),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Text(
-                          '21',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize:18.0,
+                    Card(
+                      child: ListTile(
+                        leading: Icon(Icons.contact_mail),
+                        title: Container(
+                          child: Text(
+                            user.email,
+                            style: TextStyle(fontSize: 18.0),
                           ),
                         ),
-                        SizedBox(height: 5.0),
-                        Text(
-                          'Following',
-                          style: TextStyle(
-                              fontFamily: 'Montserrat',
-                              color: Colors.grey),
-                        )
-                      ],
-                    )
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(12.0),
+                    ),
+                    Card(
+                      child: ListTile(
+                        leading: Icon(Icons.contact_phone),
+                        title: Container(
+                          width: 250,
+                          child: Text(
+                            user.phone,
+                            style: TextStyle(fontSize: 18.0),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(12.0),
+                    ),
+                    _displayButton(user),
                   ],
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.all(8.0),
-              ),
-              Card(
-                // place for user to input their name
-                child: ListTile(
-                  leading: Icon(Icons.account_circle),
-                  title: Container(
-                    child: Text(
-                      'Name',
-                      style: TextStyle(fontSize: 18.0),
-                    ),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.all(20.0),
-              ),
-              Card(
-                child: ListTile(
-                  leading: Icon(Icons.work),
-                  title: Container(
-                    child: Text(
-                      'Occupation',
-                      style: TextStyle(fontSize: 18.0),
-                    ),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.all(20.0),
-              ),
-              Card(
-                child: ListTile(
-                  leading: Icon(Icons.contact_mail),
-                  title: Container(
-                    child: Text(
-                      'Email',
-                      style: TextStyle(fontSize: 18.0),
-                    ),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.all(20.0),
-              ),
-              Card(
-                child: ListTile(
-                  leading: Icon(Icons.contact_phone),
-                  title: Container(
-                    width: 250,
-                    child: Text(
-                      'Phone',
-                      style: TextStyle(fontSize: 18.0),
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          )
-        ],
-      ),
+                )
+              ],
+            );
+          }),
     );
   }
 }
