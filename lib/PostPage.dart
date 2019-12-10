f454076d09bc24de887d62126d0d0c6bef26973f
reyesmarcosa30 <reyesmarcosa30@gmail.com>
2019-12-09 18:15:43 -0800

    Fixed Layout - Still need to order by timestamp

--- lib/PostPage.dart
+++ lib/PostPage.dart
@@ -1,22 +1,25 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:software_engineer/User.dart';
+import 'CreatePosts.dart';
+import 'constants.dart';
 
 final _firestore = Firestore.instance;
 
 class PostsPage extends StatefulWidget {
-  @override
   static final String id = 'Post_Page';
+  final String currentUserId;
+  final String userId;
+
+  PostsPage({this.currentUserId, this.userId});
+  @override
   _PostsPage createState() => _PostsPage();
 }
 
 class _PostsPage extends State<PostsPage> {
-//  void getPosts() async {
-//    final posts = await _firestore.collection('posts').getDocuments();
-//    for (var post in posts.documents) {
-//      print(post.data);
-//    }
-//  }
+
+  User userhold;
+  //final String currentUserId = currentUserId.id;
   void postsStream() async {
     await for (var snapshot in _firestore.collection('posts').snapshots()) {
       for (var post in snapshot.documents) {
@@ -24,17 +27,52 @@ class _PostsPage extends State<PostsPage> {
       }
     }
   }
-
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: <Widget>[
-          PostsStream(),
-        ])));
+        body: FutureBuilder(
+          future: usersRef.document(widget.userId).get(),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (!snapshot.hasData) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            User user = User.fromDoc(snapshot.data);
+            userhold = user;
+            //_displayButton(user);
+            return SafeArea(
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    PostsStream(),
+                  ]
+                  // _displayButton(user),
+                  ),
+            );
+          },
+          /*floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => MyCustomForm()));
+        },
+        child: Icon(Icons.add),
+      ),*/
+          //_displayButton(user),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (_) => MyCustomForm(
+                user: userhold,
+              ),
+            ),
+          ),
+          child: Icon(Icons.add),
+        ));
   }
 }
 
@@ -56,10 +94,14 @@ class PostsStream extends StatelessWidget {
           for (var post in posts) {
             final postText = post.data['text'];
             final postUser = post.data['user'];
+            final postHandle = post.data['userHandle'];
+            final postImage = post.data['imageUser'];
 
             final postBorder = PostsBorder(
               user: postUser,
               text: postText,
+              userHandle: postHandle,
+              imageUser: postImage,
             );
             postsBorders.add(postBorder);
           }
@@ -73,19 +115,62 @@ class PostsStream extends StatelessWidget {
 }
 
 class PostsBorder extends StatelessWidget {
-  PostsBorder({this.user, this.text});
+  PostsBorder({this.user, this.text, this.userHandle, this.imageUser});
   final String user;
   final String text;
+  final String userHandle;
+  final String imageUser;
+
   @override
   Widget build(BuildContext context) {
-    return Padding(
-        padding: const EdgeInsets.all(1.0),
-        child: Material(
-          color: Colors.blueGrey,
-          child: Text(
-            '$text from $user',
-            style: TextStyle(fontSize: 30.0),
+    return Container(
+      //alignment: Alignment.topLeft,
+      padding: EdgeInsets.all(10.0),
+      //padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 10.0),
+      decoration: BoxDecoration(
+          color: Color.fromRGBO(21, 32, 43, 1.0),
+          border: Border(bottom: BorderSide())),
+      child: new Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          CircleAvatar(
+            backgroundImage: NetworkImage(imageUser),
+            //'gs://fullertonmessenger.appspot.com/pikachu.jpg'),
+            //'https://firebasestorage.googleapis.com/v0/b/fullertonmessenger.appspot.com/o/pikachu.jpg?alt=media&token=5c7f880b-a315-4a46-8405-1118a8a71bef'),
           ),
-        ));
+          _tweetContent()
+        ],
+      ),
+    );
+  }
+
+  Widget _tweetContent() {
+    return Flexible(
+      child: Container(
+        margin: EdgeInsets.only(left: 10.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Text(user,
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.bold)),
+                Container(
+                  margin: EdgeInsets.only(left: 5.0),
+                  child: Text(userHandle + " · 30m",
+                      style: TextStyle(color: Colors.grey[400])),
+                )
+              ],
+            ),
+            Container(
+                margin: EdgeInsets.only(top: 5.0),
+                child: Text(text, style: TextStyle(color: Colors.white))),
+          ],
+        ),
+      ),
+    );
   }
 }
