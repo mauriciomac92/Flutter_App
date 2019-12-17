606eba8daddc3d6f5aed317f6a0c827ab6cb500d
mauriciomac92 <36291153+mauriciomac92@users.noreply.github.com>
2019-12-16 20:50:38 -0800

    changed widget

--- lib/PostPage.dart
+++ lib/PostPage.dart
@@ -1,40 +1,211 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:software_engineer/MessagePage.dart';
+import 'User_data.dart';
 import 'package:software_engineer/User.dart';
-import 'CreatePosts.dart';
+import 'edit_profile_page.dart';
+import 'DatabaseService.dart';
 import 'constants.dart';
+import 'package:provider/provider.dart';
+import 'PageLayout.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:timeago/timeago.dart' as timeago;
-//import 'package:pull_to_refresh/pull_to_refresh.dart';
-//import 'progress.dart';
 
-final _firestore = Firestore.instance;
 
-class PostsPage extends StatefulWidget {
-  static final String id = 'Post_Page';
+class ProfilePage extends StatefulWidget {
   final String currentUserId;
   final String userId;
 
-  PostsPage({this.currentUserId, this.userId});
+  ProfilePage({this.currentUserId, this.userId});
+
   @override
-  _PostsPage createState() => _PostsPage();
+  _ProfilePageState createState() => _ProfilePageState();
 }
 
-class _PostsPage extends State<PostsPage> {
-  User userhold;
-  //final String currentUserId = currentUserId.id;
-  void postsStream() async {
-    await for (var snapshot in _firestore.collection('posts').snapshots()) {
-      for (var post in snapshot.documents) {
-        print(post.data);
-      }
+class _ProfilePageState extends State<ProfilePage> {
+  bool isFollowing = false;
+  int followerCount = 0;
+  int followingCount = 0;
+  int postCount = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _setupIsFollowing();
+    _setupFollowers();
+    _setupFollowing();
+  }
+
+  _setupIsFollowing() async {
+    bool isFollowingUser = await DatabaseService.isFollowingUser(
+      currentUserId: widget.currentUserId,
+      userId: widget.userId,
+    );
+    setState(() {
+      isFollowing = isFollowingUser;
+    });
+  }
+
+  _setupFollowers() async {
+    int userFollowerCount = await DatabaseService.numFollowers(widget.userId);
+    setState(() {
+      followerCount = userFollowerCount;
+    });
+  }
+
+  _setupFollowing() async {
+    int userFollowingCount = await DatabaseService.numFollowing(widget.userId);
+    setState(() {
+      followingCount = userFollowingCount;
+    });
+  }
+
+  _followOrUnfollow() {
+    if (isFollowing) {
+      _unfollowUser();
+    } else {
+      _followUser();
     }
+  }
+
+  _unfollowUser() {
+    DatabaseService.unfollowUser(
+      currentUserId: widget.currentUserId,
+      userId: widget.userId,
+    );
+    setState(() {
+      isFollowing = false;
+      followerCount--;
+    });
+  }
+
+  _followUser() {
+    DatabaseService.followUser(
+      currentUserId: widget.currentUserId,
+      userId: widget.userId,
+    );
+    setState(() {
+      isFollowing = true;
+      followerCount++;
+    });
+  }
+
+  // showChat(User user){
+  //   user.id == Provider.of<UserData>(context).currentUserId
+  //   ? Container() : IconButton(
+  //     icon: Icon(Icons.message),
+  //     onPressed: () => Navigator.push(context, MaterialPageRoute(
+  //       builder: (_) => ChatScreen(
+  //         userId: user.id,
+  //         username: user.username,
+  //       ),
+  //     ),
+  //   ),
+  //   );
+  // }
+
+  addMessage(User user){
+    return user.id == Provider.of<UserData>(context).currentUserId
+    ? Container() : IconButton(
+      icon: Icon(Icons.message),
+      onPressed: () => addMessagePost(user)
+    );
+  }
+
+  addMessagePost(User user){
+      Firestore.instance.collection('friend')
+      .add({
+        'id': DateTime.now().millisecondsSinceEpoch,
+        'user1': Provider.of<UserData>(context).currentUserId,
+        'user2': user.id,
+        'email': user.email,
+        'time': Timestamp.now(),
+      });
+      Navigator.push(context, MaterialPageRoute(
+        builder: (_) => MainPage()
+      ),
+    );
+  }
+  
+  _displayButton(User user) {
+    return user.id == Provider.of<UserData>(context).currentUserId
+        ? Container(
+      width: 200.0,
+      child: FlatButton(
+        onPressed: () => Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (_) => EditProfilePage(
+              user: user,
+            ),
+          ),
+        ),
+        color: Colors.blue,
+        textColor: Colors.white,
+        child: Text(
+          'Edit Profile',
+          style: TextStyle(fontSize: 18.0),
+        ),
+      ),
+    )
+        : Container(
+      width: 200.0,
+      child: FlatButton(
+        onPressed: _followOrUnfollow,
+        color: isFollowing ? Colors.grey[200] : Colors.blue,
+        textColor: isFollowing ? Colors.black : Colors.white,
+        child: Text(
+          isFollowing ? 'Unfollow' : 'Follow',
+          style: TextStyle(fontSize: 18.0),
+        ),
+      ),
+    );
+  }
+
+  Column BuildStatColumn (String label, int number) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          number.toString(),
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 18.0,
+          ),
+        ),
+        SizedBox(height: 5.0),
+        Text(
+          label,
+          style: TextStyle(
+              fontFamily: 'Montserrat',
+              color: Colors.grey),
+        )
+      ],
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Posts'),centerTitle: true,),
-        body: FutureBuilder(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        // leading: IconButton(
+        //   icon: Icon(Icons.arrow_back),
+        //   onPressed: () { Navigator.pop(context); },
+        // ),
+        title: Text('Profile'),
+        // actions: <Widget>[
+        //   IconButton(
+        //     icon: Icon(Icons.message),
+        //     onPressed: () {
+        //     //addMessagePost();
+        //     }
+        //   ),
+        // ],
+        centerTitle: true,
+      ),
+      body: FutureBuilder(
           future: usersRef.document(widget.userId).get(),
           builder: (BuildContext context, AsyncSnapshot snapshot) {
             if (!snapshot.hasData) {
@@ -43,162 +214,102 @@ class _PostsPage extends State<PostsPage> {
               );
             }
             User user = User.fromDoc(snapshot.data);
-            userhold = user;
-            //_displayButton(user);
-            return SafeArea(
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: <Widget>[
-                    PostsStream(),
-                  ]
-                  // _displayButton(user),
-                  ),
-            );
-          },
-          /*floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(
-              context, MaterialPageRoute(builder: (context) => MyCustomForm()));
-        },
-        child: Icon(Icons.add),
-      ),*/
-          //_displayButton(user),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () => Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (_) => MyCustomForm(
-                user: userhold,
-              ),
-            ),
-          ),
-          child: Icon(Icons.add),
-        ));
-  }
-}
-
-class PostsStream extends StatelessWidget {
-//  final RefreshController _refreshController = RefreshController();
-  @override
-  Widget build(BuildContext context) {
-    return StreamBuilder<QuerySnapshot>(
-        stream: _firestore
-            .collection('posts')
-            .orderBy('time', descending: true)
-            .snapshots(),
-        builder: (context, snapshot) {
-          if (!snapshot.hasData) {
-            return Center(
-              child: CircularProgressIndicator(
-                backgroundColor: Colors.lightBlueAccent,
-              ),
-            );
-          }
-          final posts = snapshot.data.documents;
-          List<PostsBorder> postsBorders = [];
-          for (var post in posts) {
-            final postText = post.data['text'];
-            final postUser = post.data['user'];
-            final postHandle = post.data['userHandle'];
-            final postImage = post.data['imageUser'];
-            final postTime = post.data['time'];
-
-            final postBorder = PostsBorder(
-              user: postUser,
-              text: postText,
-              userHandle: postHandle,
-              imageUser: postImage,
-              time: postTime,
-            );
-            postsBorders.add(postBorder);
-          }
-          /*SmartRefresher(
-            controller: _refreshController,
-            enablePullUp: true,
-            enablePullDown: true,
-            onRefresh: () {
-              _PostsPage();
-            },
-          );*/
-          return Expanded(
-            child: ListView(
-              children: postsBorders,
-            ),
-          );
-        });
-  }
-}
-
-class PostsBorder extends StatelessWidget {
-  PostsBorder(
-      {this.user, this.text, this.userHandle, this.imageUser, this.time});
-  final String user;
-  final String text;
-  final String userHandle;
-  final String imageUser;
-  final Timestamp time;
-
-  //var date = new DateTime.fromMillisecondsSinceEpoch(time * 1000);
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      //alignment: Alignment.topLeft,
-      //padding: EdgeInsets.all(10.0),
-      //padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 10.0),
-      //decoration: BoxDecoration(
-          //color: Color.fromRGBO(21, 32, 43, 1.0),
-          //border: Border(bottom: BorderSide())),
-      child: new Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          CircleAvatar(
-            backgroundImage: NetworkImage(imageUser),
-            radius: 25.0,
-
-            //'gs://fullertonmessenger.appspot.com/pikachu.jpg'),
-            //'https://firebasestorage.googleapis.com/v0/b/fullertonmessenger.appspot.com/o/pikachu.jpg?alt=media&token=5c7f880b-a315-4a46-8405-1118a8a71bef'),
-          ),
-          _tweetContent()
-        ],
-      ),
-    );
-  }
-
-  Widget _tweetContent() {
-    return Flexible(
-      child: Container(
-        margin: EdgeInsets.only(left: 10.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            Row(
+            return ListView(
               children: <Widget>[
-                Text(user,
-                    style: TextStyle(
-                        color: Colors.white, fontWeight: FontWeight.bold)),
-                Container(
-                  margin: EdgeInsets.only(left: 5.0),
-                  child: Text(userHandle,
-                      style: TextStyle(color: Colors.grey[400])),
-                )
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Center(
+                      child: CircleAvatar(
+                        // place for user to add their avatar
+                        radius: 70,
+                        backgroundColor: Colors.grey,
+                        backgroundImage: user.profileImageUrl.isEmpty
+                            ? AssetImage('assets/images/user_placeholder.jpg')
+                            : CachedNetworkImageProvider(user.profileImageUrl),
+                      ),
+                    ),
+                    SizedBox(height: 25.0),
+                    Text(
+                      user.username,
+                      style: TextStyle(
+                          fontFamily: 'Montserrat',
+                          fontSize: 30.0,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 4.0),
+                    Text(
+                      user.bio,
+                      style: TextStyle(
+                          fontFamily: 'Montserrat',
+                          color: Colors.grey,
+                          fontSize: 18.0),
+                    ),
+                    _displayButton(user),
+                    addMessage(user),
+                    Padding(
+                      padding: EdgeInsets.all(15.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          BuildStatColumn("Posts", postCount),
+                          BuildStatColumn("Follower", followerCount),
+                          BuildStatColumn("Following", followingCount),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(4.0,4.0,4.0,0.0),
+                    ),
+                    Card(
+                      // place for user to input their name
+                      child: ListTile(
+                        leading: Icon(Icons.account_circle),
+                        title: Container(
+                          child: Text(
+                            user.name,
+                            style: TextStyle(fontSize: 18.0),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(5.0),
+                    ),
+                    Card(
+                      child: ListTile(
+                        leading: Icon(Icons.contact_mail),
+                        title: Container(
+                          child: Text(
+                            user.email,
+                            style: TextStyle(fontSize: 18.0),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(5.0),
+                    ),
+                    Card(
+                      child: ListTile(
+                        leading: Icon(Icons.contact_phone),
+                        title: Container(
+                          width: 250,
+                          child: Text(
+                            user.phone,
+                            style: TextStyle(fontSize: 18.0),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(5.0),
+                    ),
+                  ],
+                ),
               ],
-            ),
-            Container(
-                margin: EdgeInsets.only(top: 5.0),
-                child: Text(text, style: TextStyle(color: Colors.white))),
-            Container(
-              margin: EdgeInsets.only(top: 5.0),
-              child: Text(timeago.format(time.toDate()),
-                  style: TextStyle(color: Colors.grey[400])),
-            ),
-          ],
-        ),
-      ),
+            );
+          }),
     );
   }
 }
